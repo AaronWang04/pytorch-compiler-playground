62e85f3cfdf43e4b96da88bebbf80069aa3bc141
AaronWang04 <aaronw23880@gmail.com>
2024-10-16 15:23:17 -0400

    works but need to link python path

--- csrc/operators.cpp
+++ csrc/operators.cpp
@@ -1,17 +1,26 @@
 #include <pybind11/pybind11.h>
 
-int add(int i, int j) {
+double add(double i, double j) {
     return i + j;
 }
 
-int mul(int i, int j){
+double subtract(double i, double j) {
+    return i - j;
+}
+
+double mul(double i, double j){
     return i * j;
 }
 
+double divide(double i, double j){
+    return i / j;
+}
 
 
 PYBIND11_MODULE(operators, m) {
     // expose functions
     m.def("add", &add, "A function that adds two numbers");
     m.def("mul", &mul, "A function that multiplies two numbers");
+    m.def("subtract", &subtract, "A function that subtracts two numbers");
+    m.def("divide", &divide, "A function that divides two numbers");
 }
--- tests/pybind_test.py
+++ tests/pybind_test.py
@@ -1,8 +1,10 @@
-import operator
+import operators
 
-if __name__ == "__main__":
-    addition = operator.add(3, 4)
-    assert addition == 7
-    product = operator.mul(3, 4)
-    assert product == 12
-    print("Addition and product are correct")
+addition = operators.add(3.5, 4)
+assert addition == 7.5
+subtraction = operators.subtract(4, 3)
+assert subtraction == 1
+product = operators.mul(3, 4)
+assert product == 12
+
+print("All operations are correct")
