b8bff4ca85bea3a01d1119bd57637c2364a5a8a3
AaronWang04 <aaronw23880@gmail.com>
2024-10-16 14:09:49 -0400

    remove libtorch

--- tests/pybind_test.py
+++ tests/pybind_test.py
@@ -1,7 +1,8 @@
 import operator
 
-addition = operator.add(3, 4)
-assert addition == 7
-product = operator.mul(3, 4)
-assert product == 12
-print("Addition and product are correct")
+if __name__ == "__main__":
+    addition = operator.add(3, 4)
+    assert addition == 7
+    product = operator.mul(3, 4)
+    assert product == 12
+    print("Addition and product are correct")
